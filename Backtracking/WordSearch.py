90417d50e35c1b2033673b770fb354d07d504cd3
Tian Feng <feng.txy@outlook.com>
2024-01-18 20:31:16 -0800

    Update WordSearch.py

--- Backtracking/WordSearch.py
+++ Backtracking/WordSearch.py
@@ -7,16 +7,16 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        visited = set()
         rows = len(board)
         cols = len(board[0])
-        
+        visited = [[0 for i in range(cols)] for j in range(rows)]  #op1 use matrx instead of set
+
         for row in range(rows):
             for col in range(cols):
                 if board[row][col] == word[0]:
-                    visited.add((row, col))
+                    visited[row][col] = 1
                     self.dfs(row, col, board, word[1:], visited)
-                    visited.remove((row, col))
+                    visited[row][col] = 0
         return self.found
 
     def dfs(self, row, col, board, word, visited):
@@ -24,25 +24,21 @@ class Solution(object):
             self.found = True
             return 
         
-
         for row_delta, col_delta in DIRCTION:
             new_row, new_col = row + row_delta, col + col_delta
 
             if not self.is_valid(new_row, new_col, board, word, visited):
                 continue
 
-            visited.add((new_row, new_col))
+            visited[row][col] = 1
             self.dfs(new_row, new_col, board, word[1:], visited)
-            visited.remove((new_row, new_col))
+            visited[row][col] = 0
         
-        return 
-
-
     def is_valid(self, row, col, board, word, visited):
-        if (row, col) in visited:
-            return False 
         if col < 0 or col >= len(board[0]):
             return False 
         if row < 0 or row >= len(board):
             return False
+        if visited[row][col] == 1:
+            return False 
         return board[row][col] == word[0] 
