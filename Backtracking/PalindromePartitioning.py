6dfcca78cf696683b5a787ad5e1ab21839beba39
Tian Feng <feng.txy@outlook.com>
2024-01-20 23:51:44 -0800

    Update PalindromePartitioning.py

--- Backtracking/PalindromePartitioning.py
+++ Backtracking/PalindromePartitioning.py
@@ -4,22 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-        substrings = []
-        self.dfs(s, [], substrings)
-        return substrings
+        subs = []
+        self.dfs(s, [], subs)
+        return subs
 
-    def dfs(self, s, substring, substrings):
+
+    def dfs(self, s, sub, subs):
         if len(s) == 0:
-            substrings.append(substring[:])
+            subs.append(sub[:])
             return 
-        for i in range(1, len(s)+1):
-            prefix = s[:i]
-            if not self.is_palindrome(prefix):
-                continue
 
-            substring.append(prefix)
-            self.dfs(s[i:], substring, substrings)
-            substring.pop()
+        for i in range(1, len(s)+1): # len(s)+1: we need to check if s[:len(s)+1] is_palindrome
+            prefix = s[:i]
+            print(i, prefix)
+            if not self.is_palindrome(prefix):
+                continue 
+
+            sub.append(prefix)
+            self.dfs(s[i:], sub, subs)
+            sub.pop()
 
     def is_palindrome(self, s):
         return s == s[::-1]
