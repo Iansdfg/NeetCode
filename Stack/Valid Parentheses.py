7717e9b45570c829e3c97283228dd66b0f1e8e3c
Tian Feng <feng.txy@outlook.com>
2024-01-24 22:35:30 -0800

    Create Valid Parentheses.py

---
+++ Stack/Valid Parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        left_right = {
+            "(" :  ")",
+            "{" :  "}",
+            "[" :  "]",
+        }
+        for char in s:
+            if char in "([{":
+                stack.append(left_right[char])
+            else:
+                if stack == []:
+                    return False 
+                top = stack.pop()
+                if top !=  char:
+                    return False 
+
+        return stack == []
+                
+
+        
