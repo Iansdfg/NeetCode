0d29d52aecb561b6097a88031b4e8ed0d7fcb1e8
Tian Feng <feng.txy@outlook.com>
2024-01-30 21:52:58 -0800

    Update Longest Repeating Character Replacement.py

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -10,27 +10,11 @@ class Solution(object):
         max_len = 0
 
         while r < len(s):
-            if s[r] in char_cnt:
-                char_cnt[s[r]] += 1 
-            else:
-                char_cnt[s[r]] = 1 
-
-            while r - l + 1 - self.get_most_frq(char_cnt) > k:
+            char_cnt[s[r]] = 1 + char_cnt.get(s[r], 0)
+            while r - l + 1 - max(char_cnt.values()) > k:
                 char_cnt[s[l]] -= 1 
                 l += 1 
-
             max_len = max(max_len, r - l + 1 )
             r += 1 
-
         return max_len
     
-    def get_most_frq(self, hash_map):
-        max_val = 0
-        for key in hash_map:
-            if hash_map[key] > max_val:
-                max_val = hash_map[key]
-        return max_val
-
-
-
-
