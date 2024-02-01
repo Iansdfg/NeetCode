3dceca5a0cc533b6faf4fb4e3f68b9969495d79f
Tian Feng <feng.txy@outlook.com>
2024-01-31 21:26:52 -0800

    Update Longest Substring Without Repeating Characters.py

--- Sliding Window/Longest Substring Without Repeating Characters.py
+++ Sliding Window/Longest Substring Without Repeating Characters.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        char_set = set()
-        l = 0
-        r = 0
+        l = r = 0
         max_len = 0
+        char_set = set()
         while r < len(s):
-            while s[r] in char_set:
+            while l < r and s[r] in char_set:
                 char_set.remove(s[l])
                 l += 1 
-            
-            char_set.add(s[r])
-            max_len = max(max_len, r - l + 1)
+
+            if s[r] not in char_set:
+                char_set.add(s[r])
+
+            max_len = max(max_len, r-l+1)
             r += 1 
         return max_len
