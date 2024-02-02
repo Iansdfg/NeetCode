4575993748f185e7d154e603e9c6dbe045eb65ef
Tian Feng <feng.txy@outlook.com>
2024-02-01 22:40:16 -0800

    Update Longest Repeating Character Replacement.py

--- Sliding Window/Longest Repeating Character Replacement.py
+++ Sliding Window/Longest Repeating Character Replacement.py
@@ -6,15 +6,17 @@ class Solution(object):
         :rtype: int
         """
         l = r = 0
-        char_cnt = dict()
         max_len = 0
-
+        char_to_cnt = dict()
         while r < len(s):
-            char_cnt[s[r]] = 1 + char_cnt.get(s[r], 0)
-            while r - l + 1 - max(char_cnt.values()) > k:
-                char_cnt[s[l]] -= 1 
+            char_to_cnt[s[r]]= char_to_cnt.get(s[r], 0) + 1 
+            while r-l+1 - max(char_to_cnt.values()) > k:
+                char_to_cnt[s[l]] -= 1 
                 l += 1 
-            max_len = max(max_len, r - l + 1 )
+
+            max_len = max(max_len, r-l+1)
             r += 1 
+
         return max_len
-    
+
+
