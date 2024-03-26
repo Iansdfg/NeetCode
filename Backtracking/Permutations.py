f8d126043e6d8ba1ff81682b411fff102ed7a204
Tian Feng <feng.txy@outlook.com>
2024-03-26 14:31:51 -0700

    Update Permutations.py

--- Backtracking/Permutations.py
+++ Backtracking/Permutations.py
@@ -1,27 +1,20 @@
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        visited = set()
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
         permutes = []
+        visited = set()
         self.dfs(nums, [], permutes, visited)
         return permutes
-        
 
     def dfs(self, nums, permute, permutes, visited):
         if len(permute) == len(nums):
             permutes.append(permute[:])
             return 
-            
-        for i in range(len(nums)):
-            if i in visited:
-                continue 
 
-            visited.add(i)
+        for i in range(len(nums)):
+            if nums[i] in visited:
+                continue
+            visited.add(nums[i])
             permute.append(nums[i])
             self.dfs(nums, permute, permutes, visited)
             permute.pop()
-            visited.remove(i)
-        
+            visited.remove(nums[i])
