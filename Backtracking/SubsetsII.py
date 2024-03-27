1e068c04bca3c75a5545360571e349a5ebff1942
Tian Feng <feng.txy@outlook.com>
2024-03-27 16:17:29 -0700

    Update SubsetsII.py

--- Backtracking/SubsetsII.py
+++ Backtracking/SubsetsII.py
@@ -19,3 +19,22 @@ class Solution(object):
             subset.append(nums[i])
             self.dfs(nums[i+1:], subset, subsets)
             subset.pop()
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        subsets = []
+        self.dfs(nums, 0, [], subsets)
+        return subsets
+
+    def dfs(self, nums, index, subset, subsets):
+        if index > len(nums):
+            return 
+        subsets.append(subset[:])
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, subsets)
+            subset.pop()
