64e196ac0c3257236dd4cfda4d485710572afbb1
Tian Feng <feng.txy@outlook.com>
2024-03-28 21:08:07 -0700

    Update WordSearch.py

--- Backtracking/WordSearch.py
+++ Backtracking/WordSearch.py
@@ -1,44 +1,48 @@
-DIRCTION = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-class Solution(object):
-    found = False
-    def exist(self, board, word):
-        """
-        :type board: List[List[str]]
-        :type word: str
-        :rtype: bool
-        """
+DIRCTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+class Solution:
+    res = False 
+    def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
-        visited = [[0 for i in range(cols)] for j in range(rows)]  #op1 use matrx instead of set
+        visited = [[0 for i in range(cols)] for j in range(rows)]
 
         for row in range(rows):
             for col in range(cols):
                 if board[row][col] == word[0]:
                     visited[row][col] = 1
-                    self.dfs(row, col, board, word[1:], visited)
-                    visited[row][col] = 0
-        return self.found
+                    self.dfs(board, word, word[0], 1, row, col, rows, cols, visited)
+                    visited[row][col] =0
 
-    def dfs(self, row, col, board, word, visited):
-        if len(word) == 0:
-            self.found = True
+        return self.res 
+
+    def dfs(self, board, target, curr_word, index, curr_row, curr_col, rows, cols, visited):
+        if index > len(target):
+            return  
+
+        if target == curr_word:
+            self.res = True 
             return 
-        
-        for row_delta, col_delta in DIRCTION:
-            new_row, new_col = row + row_delta, col + col_delta
+    
+        for delta_row, delta_col in DIRCTIONS:
+            next_row = curr_row + delta_row
+            next_col = curr_col + delta_col
+            if not self.is_valid(board, target, index, curr_word, next_row, next_col, rows, cols, visited):
+                continue 
 
-            if not self.is_valid(new_row, new_col, board, word, visited):
-                continue
+            visited[next_row][next_col] = 1
+            curr_word += board[next_row][next_col]
+            res = self.dfs(board, target, curr_word, index + 1, next_row, next_col, rows, cols, visited)
+            curr_word = curr_word[:-1]
+            visited[next_row][next_col] = 0
 
-            visited[row][col] = 1
-            self.dfs(new_row, new_col, board, word[1:], visited)
-            visited[row][col] = 0
-        
-    def is_valid(self, row, col, board, word, visited):
-        if col < 0 or col >= len(board[0]):
+
+    def is_valid(self, board, target, index, curr_word, row, col, rows, cols, visited):
+        if row < 0 or row >= rows:
             return False 
-        if row < 0 or row >= len(board):
+        if col < 0 or col >= cols:
             return False
-        if visited[row][col] == 1:
+        if board[row][col] != target[index]:
             return False 
-        return board[row][col] == word[0] 
+        return visited[row][col] == 0 
+
+
