5a194e8d78850f352ca1f7a51bc97d6a622e3c2e
Tian Feng <feng.txy@outlook.com>
2024-03-29 17:09:47 -0700

    Create Daily Temperatures.py

---
+++ Stack/Daily Temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0 for i in range(len(temperatures))]
+        stack = []
+        for day, temp in enumerate(temperatures):
+
+            while stack and temp > stack[-1][1]:
+                old_day, old_temp = stack.pop()
+                answer[old_day] = day- old_day
+            stack.append((day, temp))
+        return answer
+
+            
