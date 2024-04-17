dc97c59aad90365bdf922dd756014a361cce7f92
Tian Feng <feng.txy@outlook.com>
2024-04-16 21:46:17 -0400

    Update Max Area of Island.py

--- Graphs/Max Area of Island.py
+++ Graphs/Max Area of Island.py
@@ -1,42 +1,41 @@
-DIRECTION = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 from collections import deque
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+class Solution:
+    rows = 0
+    cols = 0
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.rows = len(grid)
+        self.cols = len(grid[0])
         max_area = 0
-        for row in range(rows):
-            for col in range(cols):
+
+        for row in range(self.rows):
+            for col in range(self.cols):
                 if grid[row][col] == 1:
                     area = self.bfs(grid, row, col)
-                    max_area = max(area, max_area)
+                    max_area = max(max_area, area)
         return max_area
 
-    def bfs(self, grid, x, y):
-        area = 0
-        queue = deque([(x, y)])
-        grid[x][y] = 0
-        while queue:
-            curr_x, curr_y = queue.popleft()
-            area += 1
-            for delta_x, delta_y in DIRECTION:
-                next_x, next_y = delta_x + curr_x, delta_y + curr_y
-                if self.is_valid(grid, next_x, next_y):
-                    grid[next_x][next_y] = 0
-                    queue.append((next_x, next_y))
+    def bfs(self, grid, row, col):
+        queue = deque([(row, col)])
+        grid[row][col] = 0
+        area = 1
 
+        while queue: 
+            curr_row, curr_col = queue.popleft()
+            for delta_row, delta_col in DIR:
+                next_row = delta_row + curr_row
+                next_col = delta_col + curr_col
+                if self.is_valid(grid, next_row, next_col):
+                    queue.append((next_row, next_col))
+                    area += 1 
+                    grid[next_row][next_col] = 0
         return area
-    
-    def is_valid(self, grid, x, y):
-        rows = len(grid)
-        cols = len(grid[0])
 
-        if x < 0 or x >= rows:
+    def is_valid(self, grid, row, col):
+        if row < 0 or row >= self.rows:
             return False 
-        if y < 0 or y >= cols:
+        if col < 0 or col >= self.cols:
             return False 
-        return grid[x][y] == 1 
-
-
+        return grid[row][col] == 1 
         
