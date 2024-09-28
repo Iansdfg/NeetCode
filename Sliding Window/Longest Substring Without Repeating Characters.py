7cc0dacf695089c4435b365c889e6d18a6f018f4
Tian Feng <feng.txy@outlook.com>
2024-09-28 12:36:45 -0700

    Update Longest Substring Without Repeating Characters.py
    9/28/2024

--- Sliding Window/Longest Substring Without Repeating Characters.py
+++ Sliding Window/Longest Substring Without Repeating Characters.py
@@ -61,4 +61,26 @@ class Solution:
     def is_valid(self, window_len, char_cnt, k):
         return window_len - max(char_cnt.values()) <= k
 
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        if len(s) <= 1:
+            return len(s)
+
+        left, right = 0, 1
+        char_set = set([s[0]])
+        max_len = 1
+
+        while right < len(s):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1 
+
+            max_len = max(max_len, right - left + 1)
+            char_set.add(s[right])
+            right += 1 
+
+        return max_len
+
         
